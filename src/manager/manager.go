d3a71ddbb8ec5ed1c60344efca916d05e6a154f8
MartinSollie <martin.lysvand.sollie@gmail.com>
2016-04-13 13:31:54 +0200

    New branch for manager

---
+++ src/manager/manager.go
@@ -0,0 +1,22 @@
+package manager
+
+import (
+	"fmt"
+)
+
+/*
+Events:
+Nettverk mistet heis(er)
+Nettverk fikk kontakt med en heis
+Timeout for heis
+Lokal heis har fullført etasje
+Ny ordre gis til lokal heis
+Knapp trykkes på lokal heis
+Hy ordre fra nettverk / knappetrykk fra nettverk
+
+*/
+func Run() {
+	for {
+		select {}
+	}
+}
