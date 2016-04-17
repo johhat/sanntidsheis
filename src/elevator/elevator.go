8236d756736215bedf51e67d52c2f7a7f330e69f
Martin Sollie <martin.lysvand.sollie@gmail.com>
2016-04-17 18:28:27 +0200

    Single elevator module now working (but ugly code)

--- src/elevator/elevator.go
+++ src/elevator/elevator.go
@@ -3,6 +3,7 @@ package elevator
 import (
     "time"
     "../simdriver"
+    //"fmt"
 )
 
 
@@ -24,8 +25,8 @@ const (
 
 type direction_t int
 const (
-	up direction_t = iota
-	down
+	Up direction_t = iota
+	Down
 )
 
 func Run(
@@ -39,73 +40,76 @@ func Run(
     door_timer := time.NewTimer(door_period)
     door_timer.Stop()
 
+
     state := atFloor
-    current_direction := up
-    last_passed_floor = simdriver.getFloorSensorSignal()
+    current_direction := Up
+    last_passed_floor = simdriver.GetCurrentFloor()
     if(last_passed_floor == -1){
     	//Even though the driver initialized the elevator to a valid floor, it seems to be something wrong
     }
-
     for{
     	switch(state){
     	case atFloor:
     		//Noen vil av, eller på i riktig retning
     		if internalQueue[last_passed_floor] || isOrder(last_passed_floor, current_direction){
-    			simdriver.setMotorDirection(MotorStop)
-    			simdriver.setDoorOpenLamp(true)
+    			simdriver.SetMotorDirection(simdriver.MotorStop)
+    			simdriver.SetDoorOpenLamp(true)
     			deadline_timer.Stop()
     			door_timer.Reset(door_period)
     			state = doorOpen
-    		}
-    		//Ordre framover
-    		else if isOrderAhead(last_passed_floor, current_direction){
+    		} else if isOrderAhead(last_passed_floor, current_direction){ //Ordre framover
     			switch(current_direction){
-    			case up:
-    				simdriver.setMotorDirection(MotorUp) //Kanskje bare gjøre dette hvis det endrer noe?
-    			case down:
-    				simdriver.setMotorDirection(MotorDown)
+    			case Up:
+    				simdriver.SetMotorDirection(simdriver.MotorUp) //Kanskje bare gjøre dette hvis det endrer noe?
+    			case Down:
+    				simdriver.SetMotorDirection(simdriver.MotorDown)
     			}
     			deadline_timer.Reset(deadline_period)
     			state = movingBetween
-    		}
-    		//Ordre bakover
-    		else if isOrderBehind(last_passed_floor, current_direction) || isOrder(last_passed_floor, oppositeDirection(current_direction)){
+
+    		} else if isOrderBehind(last_passed_floor, current_direction) || isOrder(last_passed_floor, oppositeDirection(current_direction)){ //Ordre bakover
     			current_direction = oppositeDirection(current_direction)
     		}
     	case doorOpen:
     		select{ //Trenger vel ikke select her?
     		case <- door_timer.C:
-    			simdriver.setDoorOpenLamp(false)
+    			simdriver.SetDoorOpenLamp(false)
                 state = atFloor
-                completed_floor <- last_passed_floor
+                //completed_floor <- last_passed_floor
                 clearOrders(last_passed_floor)
     		}
     	case movingBetween:
     		select{
     			case floor := <- floor_reached:
     				last_passed_floor = floor
-    				simdriver.setFloorIndicator(floor)
+    				simdriver.SetFloorIndicator(floor)
     				state = atFloor
 					deadline_timer.Stop()
     			case <- deadline_timer.C:
     				missed_deadline <- true
     		}
     	}
+    	time.Sleep(50 * time.Millisecond)
     }
 }
 
 func isOrderAhead(currentFloor int, direction direction_t) bool{
-	if(direction == up){
-		ordersAbove = append(upQueue[currentFloor+1:],downQueue[currentFloor+1:],internalQueue[currentFloor+1:]...)
+	if(direction == Up){
+		ordersAbove := []bool{}
+		ordersAbove = append(ordersAbove, upQueue[currentFloor+1:]...)
+		ordersAbove = append(ordersAbove, downQueue[currentFloor+1:]...)
+		ordersAbove = append(ordersAbove,internalQueue[currentFloor+1:]...)
 		for _, v := range ordersAbove{
 			if v {
 				return true
 			}
 		}
 		return false
-	}
-	else{
-		ordersBelow = append(upQueue[:currentFloor],downQueue[:currentFloor],internalQueue[:currentFloor]...)
+	} else {
+		ordersBelow := []bool{}
+		ordersBelow = append(ordersBelow, upQueue[:currentFloor]...)
+		ordersBelow = append(ordersBelow, downQueue[:currentFloor]...)
+		ordersBelow = append(ordersBelow, internalQueue[:currentFloor]...)
 		for _, v := range ordersBelow{
 			if v {
 				return true
@@ -117,19 +121,26 @@ func isOrderAhead(currentFloor int, direction direction_t) bool{
 
 func isOrderBehind(currentFloor int, direction direction_t) bool{
 	//Sjekk etter ugydig etasje
-	if(direction == down){
-		ordersAbove = append(upQueue[currentFloor+1:],downQueue[currentFloor+1:],internalQueue[currentFloor+1:]...)
+	if(direction == Down){
+		ordersAbove := []bool{}
+		ordersAbove = append(ordersAbove, upQueue[currentFloor+1:]...)
+		ordersAbove = append(ordersAbove, downQueue[currentFloor+1:]...)
+		ordersAbove = append(ordersAbove,internalQueue[currentFloor+1:]...)
 		for _, v := range ordersAbove{
 			if v {
 				return true
 			}
 		}
+		
 		return false
-	}
-	else{
-		ordersBelow = append(upQueue[:currentFloor],downQueue[:currentFloor],internalQueue[:currentFloor]...)
+	} else {
+		ordersBelow := []bool{}
+		ordersBelow = append(ordersBelow, upQueue[:currentFloor]...)
+		ordersBelow = append(ordersBelow, downQueue[:currentFloor]...)
+		ordersBelow = append(ordersBelow, internalQueue[:currentFloor]...)
 		for _, v := range ordersBelow{
 			if v {
+
 				return true
 			}
 		}
@@ -144,54 +155,51 @@ func AddOrderInternal(floor int){
 	}
 
 	internalQueue[floor] = true
+	simdriver.SetBtnLamp(floor, simdriver.Command, true)
 }
 
 func AddOrderExternal(floor int, direction direction_t){
 	switch(direction){
-	case up:
+	case Up:
 		if floor < 0 || floor > simdriver.NumFloors-2 {
 			// Handle invalid order
 			return
-		}
-		else{
+		} else {
 			upQueue[floor] = true
+			simdriver.SetBtnLamp(floor, simdriver.Up, true)
 		}
-	case down:
+	case Down:
 		if floor < 1 || floor > simdriver.NumFloors-1 {
 			// Handle invalid order
 			return
-		}
-		else{
+		} else {
 			downQueue[floor] = true
+			simdriver.SetBtnLamp(floor, simdriver.Down, true)
 		}
 	}
 }
 
 func oppositeDirection(direction direction_t) direction_t {
-	switch (direction){
-	case up:
-		return down
-	case down:
-		return up
+	if direction == Up{
+		return Down
+	} else {
+		return Up
 	}
 }
 
 func isOrder(floor int, direction direction_t) bool {
-	switch(direction){
-	case up:
+	if direction == Up{
 		if floor < 0 || floor > simdriver.NumFloors-2 {
 			// Handle invalid input
 			return false
-		}
-		else{
+		} else {
 			return upQueue[floor]
 		}
-	case down:
+	} else {
 		if floor < 1 || floor > simdriver.NumFloors-1 {
 			// Handle invalid input
 			return false
-		}
-		else{
+		} else {
 			return downQueue[floor]
 		}
 	}
@@ -201,5 +209,12 @@ func clearOrders(floor int){
 	upQueue[floor] = false
 	downQueue[floor] = false
 	internalQueue[floor] = false
+	if floor != 0 {
+		simdriver.SetBtnLamp(floor, simdriver.Down, false)
+	}
+	if floor != (simdriver.NumFloors-1) {
+		simdriver.SetBtnLamp(floor, simdriver.Up, false)
+	}
+	simdriver.SetBtnLamp(floor, simdriver.Command, false)
 }
 
