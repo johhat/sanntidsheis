555ad81de787b7c7436fd457c6828871819801c0
Martin Sollie <martin.lysvand.sollie@gmail.com>
2016-04-17 19:51:32 +0200

    Commit to continue work on laptop

--- src/manager/manager.go
+++ src/manager/manager.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"../simdriver"
 	"time"
+	"../elevator"
 )
 
 const(
@@ -12,40 +13,46 @@ const(
 	floorPassingTime float32 = 0.5
 )
 
-var upQueue [4]bool
-var downQueue [4]bool
-var internalQueue [4]bool
-
+//Pseudocode, not runnable
 func getElevWaitTime(targetFloor int, direction int) float32{
 	wtime := 0
 
+	currentOrders := elevator.getOrders()
+	//Add the new order to the order list
+	currentDirection := elevator.getDirection()
+	currentFloor := elevator.getCurrentFloor()
+
+	if targetFloor == currentFloor && direction == currentDirection{
+		return 0
+	}
+	if no
+
+	for{
+		if internalQueue[last_passed_floor] || isOrder(last_passed_floor, current_direction){
+			//Clear orders on floor
+			if orderCleared{
+				return wtime
+			}
+    		wtime += stopTime
+    	} else if isOrderAhead(last_passed_floor, current_direction){ //Ordre framover
+    		wtime += floorTravelTime
+    		currentFloor = nextFloor
+    	} else if isOrderBehind(last_passed_floor, current_direction) || isOrder(last_passed_floor, oppositeDirection(current_direction)){ //Ordre bakover
+    		current_direction = oppositeDirection(current_direction)
+    	} 
+	}
+
 	/*
 	Given that the order is assigned to this elevator
-	- Find the time until the order is finished by simulating the elevator behaviour
+	- Find the time until the order is cleared by simulating the elevator behaviour
+	- Then find best case and worst case time until the passenger will get to the 
+	  destination floor, based on the other orders in the queue
 
+	
 	
 
 
-
-	if internalQueue[last_passed_floor] || isOrder(last_passed_floor, current_direction){
-    			simdriver.SetMotorDirection(simdriver.MotorStop)
-    			simdriver.SetDoorOpenLamp(true)
-    			deadline_timer.Stop()
-    			door_timer.Reset(door_period)
-    			state = doorOpen
-    		} else if isOrderAhead(last_passed_floor, current_direction){ //Ordre framover
-    			switch(current_direction){
-    			case Up:
-    				simdriver.SetMotorDirection(simdriver.MotorUp) //Kanskje bare gjøre dette hvis det endrer noe?
-    			case Down:
-    				simdriver.SetMotorDirection(simdriver.MotorDown)
-    			}
-    			deadline_timer.Reset(deadline_period)
-    			state = movingBetween
-
-    		} else if isOrderBehind(last_passed_floor, current_direction) || isOrder(last_passed_floor, oppositeDirection(current_direction)){ //Ordre bakover
-    			current_direction = oppositeDirection(current_direction)
-    		} 
+	
 
 
 
