fc1a3f4ef12a25700d8b7b39dd23f127b510adfc
MartinSollie <martin.lysvand.sollie@gmail.com>
2016-04-19 11:16:25 +0200

    Added branch com

--- src/com/com.go
+++ src/com/com.go
@@ -1,15 +1,36 @@
 package com
 
+// In progress
+
 import (
 	"../networking"
 	"../simdriver"
 	"encoding/json"
+	"log"
 )
 
 type Order struct {
-	Button  simdriver.OrderButton
-	TakenBy network.ID
-	Done    bool
+	Button   simdriver.OrderButton
+	Assignee networking.ID
+}
+
+type State struct {
+	LastPassedFloor int
+	Requests        []Order
+}
+
+func DecodeClientPacket(b []byte) (ClientData, error) {
+	var result ClientData
+	err := json.Unmarshal(b, &result)
+	return result, err
+}
+
+func EncodeMasterData(m MasterData) []byte {
+	result, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
 }
 
 type LiftEvents struct {
