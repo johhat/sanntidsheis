1e87010f53f819865e93ba02d06207234ed96663
Johan Hatleskog <johan.hatleskog@gmail.com>
2016-04-27 18:10:47 +0200

    Refactor elevator module

--- src/elevator/elevator.go
+++ src/elevator/elevator.go
@@ -1,66 +1,32 @@
 package elevator
 
 import (
-	"../simdriver"
+	driver "../simdriver"
 	"time"
 )
 
-const deadline_period = 5 * time.Second
-const door_period = 3 * time.Second
-
-type FloorOrders map[int]bool
-type Orders map[simdriver.BtnType]FloorOrders
-
-type state_t int
-
 const (
-	atFloor state_t = iota
-	doorOpen
-	movingBetween
+	deadline_period = 5 * time.Second
+	door_period     = 3 * time.Second
 )
-
-type Direction_t int
-
-const (
-	Up Direction_t = iota
-	Down
-)
-
-type request_t int
-
-const (
-	IsOrderAhead = iota
-	IsOrderBehind
-)
-
-type ReadDirection struct {
-	Floor     int
-	Direction Direction_t
-	Request   request_t
-	Resp      chan bool
-}
-type ReadOrder struct {
-	Order simdriver.ClickEvent
-	Resp  chan bool
-}
 
 var current_direction Direction_t = Up
 
-func GetCurrentDirection() Direction_t{
+func GetCurrentDirection() Direction_t {
 	return current_direction
 }
 
 func Run(
-	completed_floor 	chan<- int,
-	elev_error		 	chan<- bool,
-	floor_reached 		<-chan int,
-	new_order 			<-chan simdriver.ClickEvent,
-	new_direction 		chan<- Direction_t,
-	door_closed_chan	chan<- bool,
-	readDirs			chan<- ReadDirection,
-	ReadOrders			chan<- ReadOrder,
-	start_moving		chan<- bool,
-	passingFloor_chan	chan<- bool) {
+	completed_floor chan<- int,
+	elev_error chan<- bool,
+	floor_reached <-chan int,
+	new_order <-chan driver.ClickEvent,
+	new_direction chan<- Direction_t,
+	door_closed_chan chan<- bool,
+	readDirs chan<- ReadDirection,
+	ReadOrders chan<- ReadOrder,
+	start_moving chan<- bool,
+	passingFloor_chan chan<- bool) {
 
 	reply_chan := make(chan bool)
 
@@ -71,7 +37,7 @@ func Run(
 	door_timer.Stop()
 
 	state := atFloor
-	last_passed_floor := simdriver.GetCurrentFloor()
+	last_passed_floor := driver.GetCurrentFloor()
 	if last_passed_floor == -1 {
 		//Even though the driver initialized the elevator to a valid floor, it seems to be something wrong
 		//Run init again or crash the program?
@@ -83,15 +49,15 @@ func Run(
 		switch state {
 		case atFloor:
 			//Noen vil av, eller på i riktig retning
-			ReadOrders <- ReadOrder{simdriver.ClickEvent{last_passed_floor, simdriver.Command}, reply_chan}
+			ReadOrders <- ReadOrder{driver.ClickEvent{last_passed_floor, driver.Command}, reply_chan}
 			internal0rderAtThisFloor := <-reply_chan
-			ReadOrders <- ReadOrder{simdriver.ClickEvent{last_passed_floor, current_direction.toBtnType()}, reply_chan}
+			ReadOrders <- ReadOrder{driver.ClickEvent{last_passed_floor, current_direction.toBtnType()}, reply_chan}
 			orderForwardAtThisFloor := <-reply_chan
 
 			if internal0rderAtThisFloor || orderForwardAtThisFloor {
 				passingFloor = false
-				simdriver.SetMotorDirection(simdriver.MotorStop)
-				simdriver.SetDoorOpenLamp(true)
+				driver.SetMotorDirection(driver.MotorStop)
+				driver.SetDoorOpenLamp(true)
 				completed_floor <- last_passed_floor
 				deadline_timer.Stop()
 				door_timer.Reset(door_period)
@@ -104,14 +70,14 @@ func Run(
 
 			if orderAhead {
 				start_moving <- true
-				if(passingFloor){
+				if passingFloor {
 					passingFloor_chan <- true
 				}
 				switch current_direction {
 				case Up:
-					simdriver.SetMotorDirection(simdriver.MotorUp)
+					driver.SetMotorDirection(driver.MotorUp)
 				case Down:
-					simdriver.SetMotorDirection(simdriver.MotorDown)
+					driver.SetMotorDirection(driver.MotorDown)
 				}
 				deadline_timer.Reset(deadline_period)
 				state = movingBetween
@@ -121,7 +87,7 @@ func Run(
 
 			readDirs <- ReadDirection{last_passed_floor, current_direction, IsOrderBehind, reply_chan}
 			orderBehind := <-reply_chan
-			ReadOrders <- ReadOrder{simdriver.ClickEvent{last_passed_floor, current_direction.OppositeDirection().toBtnType()}, reply_chan}
+			ReadOrders <- ReadOrder{driver.ClickEvent{last_passed_floor, current_direction.OppositeDirection().toBtnType()}, reply_chan}
 			orderBackwardAtThisFloor := <-reply_chan
 
 			if orderBehind || orderBackwardAtThisFloor {
@@ -130,14 +96,14 @@ func Run(
 
 		case doorOpen:
 			<-door_timer.C
-			simdriver.SetDoorOpenLamp(false)
+			driver.SetDoorOpenLamp(false)
 			state = atFloor
 			door_closed_chan <- true
 		case movingBetween:
 			select {
 			case floor := <-floor_reached:
 				last_passed_floor = floor
-				simdriver.SetFloorIndicator(floor)
+				driver.SetFloorIndicator(floor)
 				state = atFloor
 				deadline_timer.Stop()
 			case <-deadline_timer.C:
@@ -145,21 +111,5 @@ func Run(
 			}
 		}
 		time.Sleep(5 * time.Millisecond)
-	}
-}
-
-func (direction Direction_t) OppositeDirection() Direction_t {
-	if direction == Up {
-		return Down
-	} else {
-		return Up
-	}
-}
-
-func (direction Direction_t) toBtnType() simdriver.BtnType {
-	if direction == Up {
-		return simdriver.Up
-	} else {
-		return simdriver.Down
 	}
 }
---
+++ src/elevator/types.go
@@ -0,0 +1,58 @@
+package elevator
+
+import (
+	driver "../simdriver"
+)
+
+type FloorOrders map[int]bool
+type Orders map[driver.BtnType]FloorOrders
+
+type ReadDirection struct {
+	Floor     int
+	Direction Direction_t
+	Request   request_t
+	Resp      chan bool
+}
+
+type ReadOrder struct {
+	Order driver.ClickEvent
+	Resp  chan bool
+}
+
+type state_t int
+
+const (
+	atFloor state_t = iota
+	doorOpen
+	movingBetween
+)
+
+type request_t int
+
+const (
+	IsOrderAhead request_t = iota
+	IsOrderBehind
+)
+
+type Direction_t int
+
+const (
+	Up Direction_t = iota
+	Down
+)
+
+func (direction Direction_t) OppositeDirection() Direction_t {
+	if direction == Up {
+		return Down
+	} else {
+		return Up
+	}
+}
+
+func (direction Direction_t) toBtnType() driver.BtnType {
+	if direction == Up {
+		return driver.Up
+	} else {
+		return driver.Down
+	}
+}
